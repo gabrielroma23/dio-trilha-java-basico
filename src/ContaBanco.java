317325ea1ba6e513d8d1dd5ff7a13f445298e5e9
Gabriel <gabrielpc23015@gmail.com>
2024-07-27 11:02:59 -0300

    commit

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -1,5 +1,35 @@
-public class Main {
+import java.util.Scanner;
+
+public class ContaBanco {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner leitura = new Scanner(System.in);
+        String nome ="";
+        String sobrenome = "";
+        String nomeCliente = nome + sobrenome;
+        String agencia = "";
+        int numeroConta;
+        double saldo;
+
+        System.out.println("Bem vindo ao sistema bancário, " +
+                "vamos realizar o seu cadastro a seguir");
+
+        System.out.println("Digite seu nome: ");
+        nome = leitura.next();
+
+        System.out.println("Digite seu sobrenome:");
+        sobrenome = leitura.next();
+
+        System.out.println("Digite a agência");
+        agencia = leitura.next();
+
+        System.out.println("Digite o número da conta");
+        numeroConta = leitura.nextInt();
+
+        System.out.println("Digite o seu saldo atual: ");
+        saldo = leitura.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, " +
+                        "sua agência é "+ agencia + " conta: " + numeroConta + " e seu saldo é: "+ saldo + "já " +
+                        "está disponível para saque");
     }
 }
